13ec06184b7632f4ce5a8b291a7e47e3ae86a611
Teinishi <teinishi.bsg@gmail.com>
2025-11-18 11:53:57 +0900

    引数のVecから型付きLinkへの変換を書き直し

--- src/semantic/logic_analyzer/functions.rs
+++ src/semantic/logic_analyzer/functions.rs
@@ -14,25 +14,72 @@ fn arg1(args: &[Spanned<Expr>]) -> Result<&Spanned<Expr>, CompileErrorType> {
     }
 }
 
+fn arg1_typed<T>(
+    logic_analyzer: &mut LogicAnalyzer,
+    args: &[Spanned<Expr>],
+) -> Option<Result<T, CompileErrorType>>
+where
+    T: TryFrom<Link>,
+    <T as TryFrom<Link>>::Error: Into<CompileErrorType>,
+{
+    match arg1(args).map(|i| logic_analyzer.expr_to_component_typed(i)) {
+        Ok(Some(r)) => Some(Ok(r)),
+        Ok(None) => None,
+        Err(err) => Some(Err(err)),
+    }
+}
+
+fn arg2(args: &[Spanned<Expr>]) -> Result<(&Spanned<Expr>, &Spanned<Expr>), CompileErrorType> {
+    if args.len() != 2 {
+        //Err(CompileErrorType::InvalidArguments)
+        todo!()
+    } else {
+        let mut iter = args.iter();
+        Ok((iter.next().unwrap(), iter.next().unwrap()))
+    }
+}
+
+fn arg2_typed<T0, T1>(
+    logic_analyzer: &mut LogicAnalyzer,
+    args: &[Spanned<Expr>],
+) -> Option<Result<(T0, T1), CompileErrorType>>
+where
+    T0: TryFrom<Link>,
+    <T0 as TryFrom<Link>>::Error: Into<CompileErrorType>,
+    T1: TryFrom<Link>,
+    <T1 as TryFrom<Link>>::Error: Into<CompileErrorType>,
+{
+    match arg2(args).map(|(i0, i1)| {
+        (logic_analyzer
+            .expr_to_component_typed(i0)
+            .zip(logic_analyzer.expr_to_component_typed(i1)))
+    }) {
+        Ok(Some(r)) => Some(Ok(r)),
+        Ok(None) => None,
+        Err(err) => Some(Err(err)),
+    }
+}
+
 pub(super) fn function_calls(
     logic_analyzer: &mut LogicAnalyzer,
     name: &str,
     args: &[Spanned<Expr>],
 ) -> Option<Result<Link, CompileErrorType>> {
-    let (component, index) = match name {
-        "abs" => match arg1(args) {
-            Ok(i) => (
-                Component::Arithmetic(ArithmeticComponent::Abs {
-                    input: logic_analyzer.expr_to_component_typed(i)?,
-                }),
-                0,
-            ),
-            Err(err) => {
-                return Some(Err(err));
-            }
-        },
+    let r = match name {
+        "abs" => arg1_typed(logic_analyzer, args).map(|i| {
+            i.map(|i| {
+                (
+                    Component::Arithmetic(ArithmeticComponent::Abs { input: i }),
+                    0,
+                )
+            })
+        }),
         _ => todo!(),
     };
 
-    Some(logic_analyzer.add_component(component, index))
+    match r {
+        Some(Ok((component, index))) => Some(logic_analyzer.add_component(component, index)),
+        Some(Err(err)) => Some(Err(err)),
+        None => None,
+    }
 }
